404ef38f9abbe9ae1c5910d8e7ba201481d31918
erin collective <fix.society.github@gmail.com>
2020-06-12 10:52:45 +0200

    testdata

---
+++ app/models/testdata.js
@@ -0,0 +1,120 @@
+module.exports = {
+    panic_request : [
+        {
+            panic_id: "c35789d6-9736-4c68-9cf9-171c069eb2bc",
+            geolat: 31.0468285,
+            geolong: -29.837302,
+            request_time: "2020-06-10T22:51:34.465Z",
+            requester_id: "7d55f3df-dce3-4270-889b-a1179a20ddda",
+            resolved_time: null,
+            response_id: null,
+            createdAt: "2020-06-10T22:51:34.467Z",
+            updatedAt: "2020-06-10T22:51:34.467Z"
+        },
+        {
+            panic_id: "fbffee55-28ca-4a1b-b4b5-8e5d2a06843f",
+            geolat: 31.0468385,
+            geolong: -29.837402,
+            request_time: "2020-06-10T22:52:55.872Z",
+            requester_id: "3523ff4c-7a45-42ba-8d13-165e8d6aac63",
+            resolved_time: null,
+            response_id: null,
+            createdAt: "2020-06-10T22:52:55.873Z",
+            updatedAt: "2020-06-10T22:52:55.873Z"
+        },
+        {
+            panic_id: "139fadcc-3b3d-4734-a71a-1bf77d2e669c",
+            geolat: 31.0468085,
+            geolong: -29.837602,
+            request_time: "2020-06-10T22:53:26.812Z",
+            requester_id: "3aa5f5da-53d2-422a-a8f4-757566219075",
+            resolved_time: null,
+            response_id: null,
+            createdAt: "2020-06-10T22:53:26.812Z",
+            updatedAt: "2020-06-10T22:53:26.812Z"
+        },
+        {
+            panic_id: "e5cdf353-a63a-4f0c-a9e5-aa85b40ebd46",
+            geolat: 31.0467585,
+            geolong: -29.838002,
+            request_time: "2020-06-10T22:53:50.279Z",
+            requester_id: "d21726a2-7d2b-40b8-8cad-efb0a54cb363",
+            resolved_time: null,
+            response_id: null,
+            createdAt: "2020-06-10T22:53:50.279Z",
+            updatedAt: "2020-06-10T22:53:50.279Z"
+        },
+        {
+            panic_id: "a3de5f0f-8e3b-4ebd-a4a2-71bc78028333",
+            geolat: 31.0467085,
+            geolong: -29.838402,
+            request_time: "2020-06-10T22:54:53.775Z",
+            requester_id: "f82f42ec-8bea-47d8-a83c-cfdb19f23bbc",
+            resolved_time: null,
+            response_id: null,
+            createdAt: "2020-06-10T22:54:53.775Z",
+            updatedAt: "2020-06-10T22:54:53.775Z"
+        },
+        {
+            panic_id: "492f2d89-df81-4aaf-8772-e29b95a02b5b",
+            geolat: 31.0467085,
+            geolong: -29.838402,
+            request_time: "2020-06-11T17:00:29.825Z",
+            requester_id: "f82f42ec-8bea-47d8-a83c-cfdb19f23bbc",
+            resolved_time: null,
+            response_id: null,
+            createdAt: "2020-06-11T17:00:29.825Z",
+            updatedAt: "2020-06-11T17:00:29.825Z"
+        }
+    ],
+    requester : [
+        {
+            requester_id: "7d55f3df-dce3-4270-889b-a1179a20ddda",
+            full_name: "test the first",
+            short_name: "first",
+            cell_number: "0981234567",
+            account_created: "2020-06-11T17:06:35.871Z",
+            updatedAt: "2020-06-11T17:06:35.871Z",
+            createdAt: "2020-06-11T17:06:35.871Z"
+        },
+        {
+            requester_id: "3523ff4c-7a45-42ba-8d13-165e8d6aac63",
+            full_name: "test the second",
+            short_name: "second",
+            cell_number: "0981234567",
+            account_created: "2020-06-11T17:06:35.871Z",
+            updatedAt: "2020-06-11T17:06:35.871Z",
+            createdAt: "2020-06-11T17:06:35.871Z"
+        },
+        {
+            requester_id: "3aa5f5da-53d2-422a-a8f4-757566219075",
+            full_name: "test the third",
+            short_name: "third",
+            cell_number: "0981234567",
+            account_created: "2020-06-11T17:06:35.871Z",
+            updatedAt: "2020-06-11T17:06:35.871Z",
+            createdAt: "2020-06-11T17:06:35.871Z"
+        },
+        {
+            requester_id: "d21726a2-7d2b-40b8-8cad-efb0a54cb363",
+            full_name: "test the fourth",
+            short_name: "fourth",
+            cell_number: "0981234567",
+            account_created: "2020-06-11T17:06:35.871Z",
+            updatedAt: "2020-06-11T17:06:35.871Z",
+            createdAt: "2020-06-11T17:06:35.871Z"
+        },
+        {
+            requester_id: "f82f42ec-8bea-47d8-a83c-cfdb19f23bbc",
+            full_name: "test the fifth",
+            short_name: "fifth",
+            cell_number: "0981234567",
+            account_created: "2020-06-11T17:06:35.871Z",
+            updatedAt: "2020-06-11T17:06:35.871Z",
+            createdAt: "2020-06-11T17:06:35.871Z"
+        }
+    ]
+};
+
+
+
